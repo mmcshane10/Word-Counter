f9ec1a5637e85a7fdfb54154091e880dde2b5674
Mike <mmcshane10@gmail.com>
2019-10-04 09:18:06 -0700

    write first test method.

--- WordCounter.Tests/ModelsTests/RepeatCounter.Tests.cs
+++ WordCounter.Tests/ModelsTests/RepeatCounter.Tests.cs
@@ -7,6 +7,14 @@ namespace WordCounter.Tests
     public class RepeatCounterTests
     {
         [TestMethod]
-        
+        string userString = "The cat is old";
+        string userWord = "cat";
+
+        RepeatCounter newRepeat = new RepeatCounter (userString, userWord);
+
+        newRepeat.CountWord();
+        int output = newRepeat.WordCount;
+        Assert.AreEqual(1, output);
+
     }
 }
